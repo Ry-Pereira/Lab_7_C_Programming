07170469162c80ab6579bad2299d6de31a22e2ed
Ryan <145312422+Ry-Pereira@users.noreply.github.com>
2025-03-23 15:11:00 -0500

    Finsished beginnign prologue comments

---
+++ problem1.c
@@ -0,0 +1,12 @@
+//Program Name: Problem 1: Football Score Possibilities
+//Brief Description: This program produces all possible combinations of scoring plays given by a NFL score from the user. The program keeps going until the player puts in a input of 0 or 1.
+//Input: The input is a valid intger value from the user.
+//Ouput: The ouput will be all possible combinations of scorinh plays as long as the NFL score input is not 0 or 1. If the input is 0 or 1, the progrma will ends as result.
+//All collaborators: None
+//Other Sources:Github Copilot and ChatGPT BroCode from Youtube, Stack Overflow, and GeekforGeeks
+//Author: Ryan Antony Pereira
+//Creation Date: 3/20/2025
+//Last Modified Date: 3/23/2025
+
+//GitHub Copilot was used to help code in the sense, when I was writing my code, it woudl help finsish or complete it. A lot of the comments were initally done by Github Copilot. I went back and rewrote all comments for every single line in the program.
+//ChaTtGPT was used in code error resolve, if I had any error in the coding process, I upload my code to see what was wrong and would correct it myself.
