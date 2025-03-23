d1dc2f17c34cddcd0845491691d7e957a8139b3f
Ryan <145312422+Ry-Pereira@users.noreply.github.com>
2025-03-23 18:46:10 -0500

    Created the beginning comment

--- problem2.c
+++ problem2.c
@@ -1,12 +1,12 @@
-//Program Name: Assignment 4: Email Prioritizer Program
-//Brief Description: The main program is the entry point into the email prioritizer program. It checks the email commands text file if valid, then reads and execute  all commands execution operation.
-//Input: It takes in a email commands text file from the user for the program to continue.
-//Ouput: The ouput will be a welcome message for the user, the ouput will such as when commands of next,count,email, and email execute, based on input from the user. there will also be a goodbye message to the user that the program has ended.
+//Program Name: Problem 2: Temperature Conversion Utility
+//Brief Description: This program perfroms temperature conversions betweem Fahrenheit,Celsius, and Kelvin. It also categorizes the termperature into predefein ranges based on the value and provides a weather advisory
+//Input: Prompts the user for a temperature value, a temperature scale of the input value of Farenheit,Celsius, or Kelving and a convertion target such as Fahrenheit,Celsisu or Kelvin.
+//Ouput: The ouput will be convert the termperature of the  user's input scale and target scale, display the converted temperature, categorize it and provide a simple weather advisory based on the category
 //All collaborators: None
 //Other Sources:Github Copilot and ChatGPT BroCode from Youtube, Stack Overflow, and GeekforGeeks
 //Author: Ryan Antony Pereira
-//Creation Date: 3/8/2025
-//Last Modified Date: 3/13/2025
+//Creation Date: 3/20/2025
+//Last Modified Date: 3/23/2025
 
 //GitHub Copilot was used to help code in the sense, when I was writing my code, it woudl help finsish or complete it. A lot of the comments were initally done by Github Copilot. I went back and rewrote all comments for every single line in the program.
 //ChaTtGPT was used in code error resolve, if I had any error in the coding process, I upload my code to see what was wrong and would correct it myself.
