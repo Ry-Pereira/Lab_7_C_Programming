e939f5f48edb418d9f258d4b9b74ae69844d880e
Ryan <145312422+Ry-Pereira@users.noreply.github.com>
2025-03-23 15:03:39 -0500

    Starting Documentation

---
+++ problem2.c
@@ -0,0 +1,12 @@
+//Program Name: Assignment 4: Email Prioritizer Program
+//Brief Description: The main program is the entry point into the email prioritizer program. It checks the email commands text file if valid, then reads and execute  all commands execution operation.
+//Input: It takes in a email commands text file from the user for the program to continue.
+//Ouput: The ouput will be a welcome message for the user, the ouput will such as when commands of next,count,email, and email execute, based on input from the user. there will also be a goodbye message to the user that the program has ended.
+//All collaborators: None
+//Other Sources:Github Copilot and ChatGPT BroCode from Youtube, Stack Overflow, and GeekforGeeks
+//Author: Ryan Antony Pereira
+//Creation Date: 3/8/2025
+//Last Modified Date: 3/13/2025
+
+//GitHub Copilot was used to help code in the sense, when I was writing my code, it woudl help finsish or complete it. A lot of the comments were initally done by Github Copilot. I went back and rewrote all comments for every single line in the program.
+//ChaTtGPT was used in code error resolve, if I had any error in the coding process, I upload my code to see what was wrong and would correct it myself.
